d1959651c064aabcb0e116bc6977df71e8a7bc59
Razzwan <razvanlomov@gmail.com>
2023-10-13 22:14:54 +0300

    feat(Task1): first task implemented

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,34 +1,58 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
-import { Task1 } from '../wrappers/Task1';
+import {Blockchain, SandboxContract} from '@ton-community/sandbox';
+import {toNano} from 'ton-core';
+import {Task1} from '../wrappers/Task1';
+import '@ton-community/test-utils';
 
 describe('Task1', () => {
-    let blockchain: Blockchain;
-    let task1: SandboxContract<Task1>;
+	let blockchain: Blockchain;
+	let task1: SandboxContract<Task1>;
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        task1 = blockchain.openContract(await Task1.fromInit());
-        const deployer = await blockchain.treasury('deployer');
-        const deployResult = await task1.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task1.address,
-            deploy: true,
-            success: true,
-        });
-    });
+	beforeEach(async () => {
+		blockchain = await Blockchain.create();
+		task1 = blockchain.openContract(await Task1.fromInit());
+		const deployer = await blockchain.treasury('deployer');
+		const deployResult = await task1.send(
+			deployer.getSender(),
+			{
+				value: toNano('0.05'),
+			},
+			{
+				$$type: 'Deploy',
+				queryId: 0n,
+			}
+		);
+		expect(deployResult.transactions).toHaveTransaction({
+			from: deployer.address,
+			to: task1.address,
+			deploy: true,
+			success: true,
+		});
+	});
 
-    it('test', async () => {
-        expect(true).toBeTruthy();
-    });
+	const act = async (action: 'Add' | 'Subtract', number: bigint) => {
+		const deployer = await blockchain.treasury('deployer');
+		await task1.send(
+			deployer.getSender(),
+			{
+				value: toNano('0.05'),
+			},
+			{
+				$$type: action,
+				queryId: 0n,
+				number: number,
+			}
+		);
+	};
+
+	it('Add', async () => {
+		await act('Add', 10n);
+
+		expect(await task1.getCounter()).toBe(10n);
+	});
+
+	it('Subtract', async () => {
+		await act('Subtract', 1n);
+
+		expect(await task1.getCounter()).toBe(-1n);
+	});
 });
--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,34 +1,37 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
-import { Task2 } from '../wrappers/Task2';
+import {Blockchain, SandboxContract} from '@ton-community/sandbox';
+import {Address, toNano} from 'ton-core';
+import {Task2} from '../wrappers/Task2';
+import '@ton-community/test-utils';
 
 describe('Task2', () => {
-    let blockchain: Blockchain;
-    let task2: SandboxContract<Task2>;
+	let blockchain: Blockchain;
+	let task2: SandboxContract<Task2>;
+	let adminAddress: Address;
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        task2 = blockchain.openContract(await Task2.fromInit());
-        const deployer = await blockchain.treasury('deployer');
-        const deployResult = await task2.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task2.address,
-            deploy: true,
-            success: true,
-        });
-    });
+	beforeEach(async () => {
+		blockchain = await Blockchain.create();
+		adminAddress = Address.parse('EQD1X7wifAbsq5C_hry1R_OdW2mx0RqXy1uuRonqNMQsd-9j');
+		task2 = blockchain.openContract(await Task2.fromInit(adminAddress));
+		const deployer = await blockchain.treasury('deployer');
+		const deployResult = await task2.send(
+			deployer.getSender(),
+			{
+				value: toNano('0.05'),
+			},
+			{
+				$$type: 'Deploy',
+				queryId: 0n,
+			}
+		);
+		expect(deployResult.transactions).toHaveTransaction({
+			from: deployer.address,
+			to: task2.address,
+			deploy: true,
+			success: true,
+		});
+	});
 
-    it('test', async () => {
-    });
+	it('test', async () => {
+	});
 });
 
