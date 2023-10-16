0d7f95c77fda2601edb105da9a13f73ebd82a316
Razzwan <razvanlomov@gmail.com>
2023-10-16 13:42:08 +0300

    4

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,36 +1,116 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
-import { Task4 } from '../wrappers/Task4';
+import {Blockchain, SandboxContract, SendMessageResult} from '@ton-community/sandbox';
+import {Address, beginCell, Cell, toNano} from 'ton-core';
+import {Task4} from '../wrappers/Task4';
 import '@ton-community/test-utils';
 
 describe('Task4', () => {
-    let blockchain: Blockchain;
-    let task4: SandboxContract<Task4>;
+	let blockchain: Blockchain;
+	let task4: SandboxContract<Task4>;
+	let nft: Address;
+	let nft2: Address;
+	let owner: Address;
+	let owner2: Address;
+	let sender: Address;
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        task4 = blockchain.openContract(await Task4.fromInit());
-        const deployer = await blockchain.treasury('deployer');
-        const deployResult = await task4.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task4.address,
-            deploy: true,
-            success: true,
-        });
-    });
+	beforeEach(async () => {
+		blockchain = await Blockchain.create();
 
-    it('test', async () => {
-    });
+		const addresses = await blockchain.createWallets(5);
+		nft = addresses[0].getSender().address;
+		nft2 = addresses[1].getSender().address;
+		owner = addresses[2].getSender().address;
+		owner2 = addresses[3].getSender().address;
+		sender = addresses[4].getSender().address;
+
+		task4 = blockchain.openContract(await Task4.fromInit(1n));
+		const deployer = await blockchain.treasury('deployer');
+		const deployResult = await task4.send(
+			deployer.getSender(),
+			{
+				value: toNano('0.05'),
+			},
+			{
+				$$type: 'Deploy',
+				queryId: 0n,
+			}
+		);
+		expect(deployResult.transactions).toHaveTransaction({
+			from: deployer.address,
+			to: task4.address,
+			deploy: true,
+			success: true,
+		});
+	});
+
+	const assignOwnership = async (prevOwner: Address, cell: Cell): Promise<SendMessageResult> => {
+		const _sender = blockchain.sender(sender);
+		return await task4.send(
+			_sender,
+			{
+				value: toNano('1'),
+			},
+			{
+				$$type: 'OwnershipAssigned',
+				queryId: 0n,
+				prevOwner,
+				forwardPayload: cell,
+			}
+		);
+	};
+
+	const withdrawalNft = async (senderAddress: Address, nftForWithdrawal: Address): Promise<SendMessageResult> => {
+		const _sender = blockchain.sender(senderAddress);
+		return await task4.send(
+			_sender,
+			{
+				value: toNano('1'),
+			},
+			{
+				$$type: 'NftWithdrawal',
+				queryId: 0n,
+				nftAddress: nftForWithdrawal,
+			}
+		);
+	};
+
+	it('set data', async () => {
+		const futureTime = new Date();
+		futureTime.setDate(futureTime.getDate() + 7);
+		const futureUtc = BigInt(Math.round(futureTime.getTime() / 1000));
+		const r = await assignOwnership(owner, beginCell().storeAddress(nft).storeUint(futureUtc, 32).endCell());
+
+		expect(await task4.getTime()).toBeGreaterThanOrEqual(BigInt(7 * 24 * 60 * 60));
+
+		expect(await task4.getOwner()).toEqualAddress(owner);
+		expect(await task4.getNft()).toEqualAddress(nft);
+
+		expect(r.transactions.length).toBe(1);
+	});
+
+	it('revert nft back if already exists', async () => {
+		const futureTime = new Date();
+		futureTime.setDate(futureTime.getDate() + 7);
+		const futureUtc = BigInt(Math.round(futureTime.getTime() / 1000));
+		const r1 = await assignOwnership(owner, beginCell().storeAddress(nft).storeUint(futureUtc, 32).endCell());
+
+		expect(r1.transactions.length).toBe(1);
+
+		const r2 = await assignOwnership(owner2, beginCell().storeAddress(nft2).storeUint(futureUtc, 32).endCell());
+
+		expect(r2.transactions.length).toBe(2);
+	});
+
+	it('try to withdrawal by not owner', async () => {
+		const futureTime = new Date();
+		futureTime.setDate(futureTime.getDate() + 7);
+		const futureUtc = BigInt(Math.round(futureTime.getTime() / 1000));
+		await assignOwnership(owner, beginCell().storeAddress(nft).storeUint(futureUtc, 32).endCell());
+
+		let r = await withdrawalNft(owner2, nft);
+
+		expect(r.events[0].type).toBe("message_sent");
+		// expect((r.events[0] as any).body.toString()).toBe("message_sent");
+	});
 });
 
 
